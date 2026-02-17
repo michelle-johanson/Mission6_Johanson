babd430d73a93c3e6d279930a37a94f2c7c05f11
michelle-johanson <mjohans0@byu.edu>
2026-02-17 16:35:39 -0700

    add more comments

--- Mission6/Models/Movie.cs
+++ Mission6/Models/Movie.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
 
+// Model = Class
+
 namespace Mission6.Models
 {
-    public class Movie
+    public class Movie // Becomes a Table
     {
-        public int MovieId { get; set; }
+        public int MovieId { get; set; } // Allows all info to have a getter and a setter
 
         [Required]
         public string Title { get; set; } = string.Empty;
@@ -13,7 +15,7 @@ namespace Mission6.Models
         public string Category { get; set; } = string.Empty;
 
         [Required]
-        public string Year { get; set; } = string.Empty;
+        public string Year { get; set; } = string.Empty; // Stored as string to make ranges easy
 
         [Required]
         public string Director { get; set; } = string.Empty;
@@ -21,11 +23,12 @@ namespace Mission6.Models
         [Required]
         public string Rating { get; set; } = string.Empty;
 
-        public bool Edited { get; set; }
+        public bool Edited { get; set; } // True False
 
         public string? LentTo { get; set; }
 
         [MaxLength(25)]
         public string? Notes { get; set; }
+        
     }
 }
